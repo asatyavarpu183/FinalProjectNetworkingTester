bb73a7792d877b3e08a3b283f9b9fad43cc711af
AyushSat <themessimess@gmail.com>
2019-05-23 19:58:53 -0700

    back to non livea citon

--- CarromNetworking/src/TesterNetworked.java
+++ CarromNetworking/src/TesterNetworked.java
@@ -214,6 +214,8 @@ public class TesterNetworked extends PApplet implements NetworkListener{
 					striker.setLoc(player.getHitarea().getX()+player.getHitarea().getWidth()/2, player.getHitarea().getY()+player.getHitarea().getHeight()/2);
 					turnPhase = 0;
 					playerTurn = (playerTurn+1) % players.size();
+					nm.sendMessage(NetworkDataObject.MESSAGE,PLAYER_MOVE, pieces, players, playerTurn, striker);
+
 				}
 			}
 			
@@ -223,12 +225,7 @@ public class TesterNetworked extends PApplet implements NetworkListener{
 			fill(0);
 			textAlign(CENTER,CENTER);
 			//text("Player 1 score: " + players.get(0).getScore() + "                      Player 2 score: " + players.get(1).getScore(),width/2,height/10);
-			try {
-				nm.sendMessage(NetworkDataObject.MESSAGE,PLAYER_MOVE, pieces, players, playerTurn, striker);
-
-			}catch(NullPointerException e) {
-				
-			}
+			
 			processNetworkMessages();
 		//}
 		
